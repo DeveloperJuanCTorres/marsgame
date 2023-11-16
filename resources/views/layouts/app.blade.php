7546a02e54f7563d94e98fd84b33bd944166187c
juan torres <juanonfleekmedia@gmail.com>
2023-11-16 14:49:19 -0500

    version1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <meta name="theme-color" content="#ffffff">
 
     <?php
-        $version = '1993.0.8';
+        $version = '1993.0.9';
     ?>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
