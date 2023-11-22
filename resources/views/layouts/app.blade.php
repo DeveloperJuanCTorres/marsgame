518367f8db72c83e80400f7ddee5691a6d1b805f
juan torres <juanonfleekmedia@gmail.com>
2023-11-22 14:30:28 -0500

    witdh

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <meta name="theme-color" content="#ffffff">
 
     <?php
-        $version = '1993.1.9';
+        $version = '1993.2.0';
     ?>
 <link rel="stylesheet" href="{{asset('assets1/css/templatemo-cyborg-gaming.css')}}">
     <link rel="preconnect" href="https://fonts.gstatic.com">
