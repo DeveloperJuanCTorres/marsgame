0a89d657f2dec1ae584729e5b2364cb806dc34d4
juan torres <juanonfleekmedia@gmail.com>
2023-11-22 13:54:40 -0500

    version2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <meta name="theme-color" content="#ffffff">
 
     <?php
-        $version = '1993.1.6';
+        $version = '1993.1.7';
     ?>
 <link rel="stylesheet" href="{{asset('assets1/css/templatemo-cyborg-gaming.css')}}">
     <link rel="preconnect" href="https://fonts.gstatic.com">
