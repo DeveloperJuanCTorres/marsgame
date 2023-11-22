eaedc9271199ab7a09d4438d5cb1c3b9d09cec41
juan torres <juanonfleekmedia@gmail.com>
2023-11-22 14:27:07 -0500

    container

--- resources/views/layouts/carrousel.blade.php
+++ resources/views/layouts/carrousel.blade.php
@@ -1,22 +1,22 @@
-<div id="carouselExampleDark" class="carousel carousel-dark slide container-carrusel" data-bs-ride="carousel" 
+<div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel" 
     style="margin-top: 100px; margin-left: 20px;margin-right: 20px; border-radius: 20px;">
   
   <div class="carousel-inner" style="border-radius: 20px;">
     
     <div class="carousel-item active contenedor" data-bs-interval="10000">
-      <div class="container py-1 ben-banner"
+      <div class="container-carrusel py-1 ben-banner"
             style="background-image: url(https://marsgame.pe/assets/img/banner5.jpg)">      
       </div>
       <!-- <img src="{{asset('assets/img/banner2.jpg')}}" class="img-contenedor" alt="..."> -->
     </div>
     <div class="carousel-item" data-bs-interval="2000">
-      <div class="container py-1 ben-banner"
+      <div class="container-carrusel py-1 ben-banner"
             style="background-image: url(https://marsgame.pe/assets/img/banner3.jpg)">      
       </div>
       <!-- <img src="{{asset('assets/img/banner.jpg')}}" class="d-block w-100" alt="..."> -->
     </div>
     <div class="carousel-item">
-      <div class="container py-1 ben-banner"
+      <div class="container-carrusel py-1 ben-banner"
             style="background-image: url(https://marsgame.pe/assets/img/banner4.jpg)">      
       </div>
       <!-- <img src="{{asset('assets/img/banner.jpg')}}" class="d-block w-100" alt="..."> -->
