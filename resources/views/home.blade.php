fb7647426fd4956aa469f323371ca03ad31aa14a
juan torres <juanonfleekmedia@gmail.com>
2023-12-04 18:51:48 -0500

    ganadores

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -190,7 +190,12 @@
           <div class="row g-0">
             <div class="col-12 col-xxl-11 offset-xxl-1">
               <h2 class="text-light text-center">Últimos Ganadores</h2>
-              <div class="carousel slide" id="carouselExampleIndicators" data-bs-ride="carouse">
+              <div class="p-4">
+                <img src="{{asset('assets/img/illustrations/jugadas.png')}}" style="display: block;margin-left: auto;margin-right: auto;" width="200" alt="">
+                <h4 class="p-2 text-center text-ligth">No tenemos ganadores aún</h4>
+                <p class="text-ligth text-center" style="font-size: 16px;">Aquí podrás a nuestros ganadores.</p>
+              </div>
+              <!-- <div class="carousel slide" id="carouselExampleIndicators" data-bs-ride="carouse">
                 <div class="carousel-inner ps-md-5 ps-lg-11 py-4 py-md-5 py-lg-4 pb-lg-9">
                   <div class="carousel-item active">
                     <div class="row h-100">
@@ -264,7 +269,7 @@
                     </div>
                   </div>
                 </div>
-              </div>
+              </div> -->
             </div>
           </div>
         </div>
