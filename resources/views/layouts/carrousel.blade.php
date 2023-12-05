65f0f3fae61ad0e84009ae547a42aaa8e5e58ce1
DesarrolladorJuanCTorres <jctorresdelcastillo@gmail.com>
2023-12-04 22:29:30 -0500

    imagenes

--- resources/views/layouts/carrousel.blade.php
+++ resources/views/layouts/carrousel.blade.php
@@ -4,23 +4,29 @@
   <div class="carousel-inner" style="border-radius: 20px;">
     
     <div class="carousel-item active contenedor" data-bs-interval="10000">
-      <div class="container-carrusel py-1 ben-banner"
+      <div class="container-carrusel py-1 ben-banner movil"
             style="background-image: url(https://marsgame.pe/assets/img/banner5.jpg)">      
       </div>
-      <!-- <img src="{{asset('assets/img/banner2.jpg')}}" class="img-contenedor" alt="..."> -->
+      <div class="container-carrusel py-1 ben-banner movil-block"
+            style="height: 650px !important;background-image: url(http://127.0.0.1:8000/assets/img/banner8.jpg)">      
+      </div>
     </div>
     <div class="carousel-item" data-bs-interval="2000">
-      <div class="container-carrusel py-1 ben-banner"
+      <div class="container-carrusel py-1 ben-banner movil"
             style="background-image: url(https://marsgame.pe/assets/img/banner6.jpg)">      
       </div>
-      <!-- <img src="{{asset('assets/img/banner.jpg')}}" class="d-block w-100" alt="..."> -->
+      <div class="container-carrusel py-1 ben-banner movil-block"
+            style="height: 650px !important;background-image: url(http://127.0.0.1:8000/assets/img/banner9.jpg)">      
+      </div>
     </div>
     <div class="carousel-item">
-      <div class="container-carrusel py-1 ben-banner"
+      <div class="container-carrusel py-1 ben-banner movil"
             style="background-image: url(https://marsgame.pe/assets/img/banner7.jpg)">      
       </div>
-      <!-- <img src="{{asset('assets/img/banner.jpg')}}" class="d-block w-100" alt="..."> -->
-    </div>
+      <div class="container-carrusel py-1 ben-banner movil-block"
+            style="height: 650px !important;background-image: url(http://127.0.0.1:8000/assets/img/banner10.jpg)">      
+      </div>
+    </div>    
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
