73b75639d25fe481a2786bd72f1f9de6cd2dd5e8
juan torres <juanonfleekmedia@gmail.com>
2023-12-12 18:52:01 -0500

    is

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,7 +31,7 @@
               <div class="text-center">
                 <a class="btn btn-primary" style="color: white;border-radius: 15px;width: 250px;" href="">Quiero registrarme</a>
               </div> -->
-              <img src="{{asset('assets/img/mascota.png')}}" style="display: block;margin-left: auto;margin-right: auto;position: relative;z-index: 1;margin-bottom: -50px;" width="50%">
+              <img src="{{asset('assets/img/mascota.png')}}" style="display: block;margin-left: auto;margin-right: auto;position: relative;z-index: 1;margin-bottom: -70px;" width="50%">
               <div class="card card-bg shadow">
                 <div class="card-body p-4 p-xl-6">
                   <h2 class="text-100 text-white">Tenemos premios todas las semanas, ¡Ven y participa Aquí!</h2>
