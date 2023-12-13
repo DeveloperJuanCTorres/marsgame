b1da2ae18f98eaed1f2453547e00b2d1cd529ff9
juan torres <juanonfleekmedia@gmail.com>
2023-12-13 13:31:41 -0500

    color

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -454,7 +454,7 @@
                   <div class="p-2">
                     <h4>Recordatorio:</h4>
                     <span class="text-light">Para realizar pagos con tarjeta debes tener habilitado la opción de pagos online de tu tarjeta</span><br>
-                    <span class="text-light">"Contactar a soporte para consultar o validar el pago"</span>
+                    <span style="color: #00983A;">"Contactar a soporte para consultar o validar el pago"</span>
                   </div>
                   @if ($saldo >= Cart::getTotal())
                   <button id="botonPagarSaldo" class="btn btn-primary btn-pagar" style="width: 200px; background-color: transparent !important;color: white !important;display: none;"> Pagar (Saldo: S/. {{$saldo}})</button>
--- resources/views/depositar.blade.php
+++ resources/views/depositar.blade.php
@@ -254,7 +254,7 @@
                   <div class="p-2">
                     <h4>Recordatorio:</h4>
                     <span class="text-light">Para realizar pagos con tarjeta debes tener habilitado la opción de pagos online de tu tarjeta</span><br>
-                    <span class="text-light">"Contactar a soporte para consultar o validar el pago"</span>
+                    <span style="color: #00983A;">"Contactar a soporte para consultar o validar el pago"</span>
                   </div>
                 </div>
                 <div class="col-xl-8 col-md-12 col-sm-12" style="overflow-x:auto;">
